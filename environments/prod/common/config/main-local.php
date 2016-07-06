2f2ad70202cfe9630dcbe3b6692663505f686fb4
Matias <blinkero182@gmail.com>
2016-07-06 14:46:25 +0200

    pedido minimo

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=flotasgps_app',
+            'username' => 'flotasgps',
+            'password' => 'Cartam0vil',
             'charset' => 'utf8',
         ],
         'mailer' => [
