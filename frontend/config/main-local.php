99b8dc07a23a4cbbf22b92581ff6b65422e44e06
Matias <blinkero182@gmail.com>
2016-07-19 15:09:20 +0200

    una file

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,24 +1,9 @@
 <?php
-
-$config = [
+return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'v3ZRE1tOM_j-xdon3pcuO7KG7Su2pC2z',
+            'cookieValidationKey' => 'Q5xQFwjS9EZ7yxO_pJ0E5IaKPPguoyon',
         ],
     ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;
